66575e2ce4211bb6742d5c9abe2111c615bbc442
Begemot100 <you@example.com>
2025-03-02 01:12:32 +0100

    Добавлен gunicorn в requirements.txt

--- frontend/src/app/new/page.js
+++ frontend/src/app/new/page.js
@@ -20,7 +20,7 @@ export default function NewCollection() {
 
   const fetchNewCollection = async () => {
     try {
-        const response = await fetch("https://shop-production-3be1.up.railway.app//api/products/new", {        method: "GET",
+        const response = await fetch("https://shop-production-3be1.up.railway.app/api/products/new", {        method: "GET",
         headers: { "Content-Type": "application/json" },
       });
 
